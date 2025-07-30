8ee4b49db2ca287bff894e0b851dd6eefc3ddea3
LEBOT Quentin (EXT-RIC-CH) <quentin.lebot@richemont.com>
2025-07-30 16:46:06 +0200

    set nvim transparent

---
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+}
