6b553c75cb21bd669e7c69ef51b4f837505f2c72
LEBOT Quentin (EXT-RIC-CH) <quentin.lebot@richemont.com>
2025-08-01 10:03:17 +0200

    update pluggins

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,8 +1,10 @@
 return {
+	"christoomey/vim-tmux-navigator",
 	{
 		"nvim-lualine/lualine.nvim",
 		opts = function(_, opts)
 			opts.sections.lualine_z = {}
 		end,
 	},
+	{ "typicode/bg.nvim", lazy = false },
 }
