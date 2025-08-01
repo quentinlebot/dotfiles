952ddbc60fc3ac0f35996692c36a8baf81162bd0
LEBOT Quentin (EXT-RIC-CH) <quentin.lebot@richemont.com>
2025-08-01 15:33:04 +0200

    [nvim] setup tmux navigator

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,5 +1,15 @@
 return {
-	"christoomey/vim-tmux-navigator",
+	{
+		"christoomey/vim-tmux-navigator",
+		enabled = true,
+		event = "VeryLazy",
+		keys = {
+			{ "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate to left pane" },
+			{ "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate to down pane" },
+			{ "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate to up pane" },
+			{ "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate to right pane" },
+		},
+	},
 	{
 		"nvim-lualine/lualine.nvim",
 		opts = function(_, opts)
